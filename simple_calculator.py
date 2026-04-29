e2c34a00c886e75fbd99313629585223aa94af7f
Dianne <crisdiannecasino2@gmail.com>
2026-04-29 20:02:09 +0800

    added a exception so that it will display enter a number when input is not a float, and also added a code raise exception when the input is not 1, 2, 3, or 4 then added another exception so that when the raise exception happen it will print nvalid choice

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,19 +1,27 @@
 class UserInput:
     def choose_math_operation(self):
-        print("Choose math operation:")
-        print("1. Addition")
-        print("2. Subtraction")
-        print("3. Multiplication")
-        print("4. Division")
+        try:
+            print("Choose math operation:")
+            print("1. Addition")
+            print("2. Subtraction")
+            print("3. Multiplication")
+            print("4. Division")
 
-        choice = int(input("Enter number of your choice: "))
-        print(choice)
-        return choice
+            choice = int(input("Enter number of your choice: "))
+            if 1 <= choice <= 4:
+                print(choice)
+                return choice
+            else:
+                raise Exception("invalid choice")
+        except ValueError:
+            print("Please enter a Number")
+        except:
+            print("Invalid Choice")
+
 
     def get_two_numbers(self):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
-
 
 calculator = UserInput()
 calculator.choose_math_operation()
