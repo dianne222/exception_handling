69f4cef4e2ec5f94ed783f246ae751fb6ac68545
Dianne <crisdiannecasino2@gmail.com>
2026-05-01 01:31:11 +0800

    added a simple design border for the result that is adjusting base on the length of numbers and result

--- simple_calculator.py
+++ simple_calculator.py
@@ -18,15 +18,19 @@ class UserInput:
                     return choice
                 else:
                     raise ValueError("invalid choice")
+
             except:
                 print("Invalid Choice")
 
     def get_two_numbers(self):
         while True:
             try:
+                print("-" * 30)
                 num1 = float(input("Enter first number: "))
                 num2 = float(input("Enter second number: "))
+                print("-" * 30)
                 return num1, num2
+
             except ValueError:
                 print("Please enter a Number")
 
@@ -59,6 +63,9 @@ class SolveOperation(UserInput):
             return self.division(num1, num2)
 
 class Calculator(SolveOperation):
+    # def display_result(self):
+    #     print("\n")
+
     def calculate(self):
         continue_choice = "y"
         while continue_choice == "y":
@@ -68,7 +75,39 @@ class Calculator(SolveOperation):
             num1, num2 = self.get_two_numbers()
 
             result = self.operations(choice, num1, num2)
-            print(result)
+            len_num1 = len(str(num1)) + 21
+            len_num2 = len(str(num2)) + 21
+            if len_num1 > len_num2:
+                border = "-" * len_num1
+            else:
+                border = "-" * len_num2
+            if len(str(num1)) > len(str(num2)):
+                if ((len(str(num1)) - len(str(num2))) % 2) != 0:
+                    spacing2 = " " * (int((len(str(num1)) - len(str(num2))) / 2) + 1)
+                    spacing2a = " " * int((len(str(num1)) - len(str(num2))) / 2)
+                else:
+                    spacing2 = " " * int((len(str(num1)) - len(str(num2))) / 2)
+                    spacing2a = spacing2
+                spacing1 = " " * 0
+                spacing1a = " " * 0
+                print(spacing2)
+            else:
+                if ((len(str(num2)) - len(str(num1))) % 2) != 0:
+                    spacing1 = " " * (int((len(str(num2)) - len(str(num1))) / 2) + 1)
+                    spacing1a = " " * int((len(str(num2)) - len(str(num1))) / 2)
+                else:
+                    spacing1 = " " * int((len(str(num2)) - len(str(num1))) / 2)
+                    spacing1a = spacing1
+                spacing2 = " " * 0
+                spacing2a = " " * 0
+
+            print(border)
+            print(f"| First Number:  | {spacing1a}{num1}{spacing1} |")
+            print(border)
+            print(f"| Second Number: | {spacing2a}{num2}{spacing2} |")
+            print(border)
+            print(f"| Result:        | {result} |" )
+            print(border)
 
             while True:
                 continue_choice = input("Do you want to continue?(y/n): ").lower().strip()
